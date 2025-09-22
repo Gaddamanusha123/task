d1b55973ba84444a2029ed29bdcbd31f588b9393
Gaddamanusha123 <anusha6113@gmail.com>
2025-09-22 16:51:29 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number: "))
+i = 1
+count = 0
+while count < x:
+    print(i, end=", " if count < x-1 else "\n")
+    i += 2
+    count += 1
---
+++ problem3.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a number: "))
+if a % 2 == 0:
+    count = a - 1
+else:
+    count = a
+i = 0
+num = 1
+while i < count:
+    print(num, end="")
+
+    if i < count - 1:
+        print(", ", end="")
+
+    num = num + 2
+    i = i + 15
+    
